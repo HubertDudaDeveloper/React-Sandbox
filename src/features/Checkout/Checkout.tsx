83ddea3868fc30040a2a62f577049d1b36c2b300
HubertDudaDeveloper <102989117+HubertDudaDeveloper@users.noreply.github.com>
2025-05-07 20:50:57 +0200

    Create Checkout.tsx

---
+++ src/features/Checkout/Checkout.tsx
@@ -0,0 +1,15 @@
+export const Checkout = () => {
+  return (
+    <div>
+      <form>
+        <input/>
+        <input/>
+        <input/>
+        <input/>
+        <input/>
+        <input/>
+        <input/>
+      </form>
+    </div>
+  )
+}
